27589f7f04b10f26842cba3d50d2fd2b153363e8
Eric <eric.crahen@gmail.com>
2014-09-12 13:25:12 -0700

    Provision vbox to use id_vargrant

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,7 @@ Vagrant.require_version ">= 1.6"
 AMAZON_IMAGE ||= "ami-d13845e1"
 AMAZON_INSTANCE_TYPE ||= "t2.micro"
 GOOGLE_IMAGE ||= 'debian-7-wheezy-v20140619'
-GOOGLE_INSTANCE_TYPE| = 'n1-standard-1'
+GOOGLE_INSTANCE_TYPE ||= 'n1-standard-1'
 
 
 # Create minimal VMs with Docker installed
@@ -20,12 +20,14 @@ Vagrant.configure("2") do |config|
 
       # Resource Configuration
       virtualbox.customize ["modifyvm", :id, "--cpuexecutioncap", "50"]
+      virtualbox.customize ["storagectl", :id, "--name", "IDE_0", "--add", "ide"]
+      virtualbox.customize ["storageattach", :id, "--storagectl", "IDE_0", "--port", "0", "--device", "0", "--type", "dvddrive", "--medium", "my-user-data.iso"]
 
       # SSH
       override.ssh.username = VAGRANT_SSH_USERNAME
       override.ssh.forward_agent = false
       override.ssh.forward_x11 = true
-      #override.ssh.private_key_path = File.expand_path(VAGRANT_SSH_KEY)
+      override.ssh.private_key_path = File.expand_path(VAGRANT_SSH_KEY)
       #override.ssh.pty = true
 
     end
--- bin/vagrant-bootstrap.sh
+++ bin/vagrant-bootstrap.sh
@@ -44,3 +44,30 @@ vagrant_google_bootstrap() {
   vagrant box add gce https://github.com/mitchellh/vagrant-google/raw/master/google.box
 }
 vagrant plugin list | grep -q vagrant-google || vagrant_google_bootstrap
+
+
+###############################
+# Bootstrap VirtualBox Support
+vagrant_virtualbox_bootstrap() {
+  # Create an ISO to mount w/ virtualbox machine that contains cloud-config
+  # that bootstraps the instance.
+  # Install the public key from: ~/.ssh/id_vagrant.pub
+  localds_url="http://bazaar.launchpad.net/~cloud-utils-dev/cloud-utils/trunk/download/head:/cloudlocalds-20120823015036-zkgo0cswqhhvener-1/cloud-localds"
+  which cloud-localds ||
+  { sudo wget "$localds_url" -O /usr/bin/cloud-localds &&
+    sudo chmod 755 /usr/bin/cloud-localds; }
+  config_iso="my-user-data.iso"
+  if [ ! -e $config_iso ]; then
+cat > my-user-data <<EOF
+#cloud-config
+#default_user:
+#  name: vagrant
+#  ssh_authorized_keys:
+#    - $(cat $HOME/.ssh/id_vagrant.pub)
+runcmd:
+  - [ sh, "-c", "echo \"$(cat $HOME/.ssh/id_vagrant.pub)\" > /home/vagrant/.ssh/authorized_keys; chown vagrant:vagrant /home/vagrant/.ssh/authorized_keys; chmod 644 /home/vagrant/.ssh/authorized_keys;" ]
+EOF
+    cloud-localds "$config_iso" my-user-data && rm my-user-data
+  fi
+}
+vagrant_virtualbox_bootstrap
